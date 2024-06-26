31db867a8e1929942578d6d7593579f53f6bb3f1
Lomenzo <LomenzoJVM@gmail.com>
2024-06-26 17:22:21 +0800

    1-4 done

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,20 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("Vasiliy", "Petrovich", "user1@mail.ru");
+      User user2 = new User("Пахан", "Местный", "user2@mail.ru");
+      User user3 = new User("Иван", "Мажоров", "user3@mail.ru");
+      User user4 = new User("Санёк", "Настолбович", "user4@mail.ru");
+
+      user1.setCar(new Car("VAZ", 2106));
+      user2.setCar(new Car("Mercedes", 600));
+      user3.setCar(new Car("Ferrari", 50));
+      user4.setCar(new Car("ToyotaMark", 2));
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,9 +37,8 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
-         System.out.println();
+         System.out.println(user.getCar());
       }
-
       context.close();
    }
 }
